4b899b9bef3642b4ca9669b675c38978639c2914
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-08-07 19:33:16 +0700

    initializing tauri with react and vite

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          typescript
+          typescript-language-server
+          nodejs_24
+          pkg-config
+          gobject-introspection
+          cargo-tauri
+          rustup
+
+          # build input
+          at-spi2-atk
+          atkmm
+          cairo
+          gdk-pixbuf
+          glib
+          gtk3
+          harfbuzz
+          librsvg
+          libsoup_3
+          pango
+          webkitgtk_4_1
+          openssl
+        ];
+
+        shellHook = ''
+          exec zsh
+        '';
+      };
+  };
+}
